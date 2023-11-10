352e642696cef4e35c021c86b1dc614bce6ee63e
Reke <rekeojobor2@gmail.com>
2023-11-10 15:38:20 +0000

    Console.logged the left and right half of my sliced array

--- mergesort.js
+++ mergesort.js
@@ -8,6 +8,9 @@ function mergeSort(array)
         const mid = Math.floor(array.length / 2);
         const leftHalf = array.slice(0, mid);
         const rightHalf = array.slice(mid);
+
+        console.log(leftHalf)
+        console.log(rightHalf)
     }
 }
 

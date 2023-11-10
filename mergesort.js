dcc011c097747861152f632416258813b6f0a511
Reke <rekeojobor2@gmail.com>
2023-11-10 15:50:01 +0000

    Completed the code

--- mergesort.js
+++ mergesort.js
@@ -9,10 +9,39 @@ function mergeSort(array)
         const leftHalf = array.slice(0, mid);
         const rightHalf = array.slice(mid);
 
-        console.log(leftHalf)
-        console.log(rightHalf)
+        // console.log(leftHalf)
+        // console.log(rightHalf)
+
+        const leftSorted = mergeSort(leftHalf);
+        const rightSorted = mergeSort(rightHalf)
+        
+        return merge(leftSorted, rightSorted);
     }
 }
+
+function merge(left, right)
+{
+    let result = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < left.length && j < right.length)
+    {
+        if (left[i] < right[j])
+        {
+            result.push(left[i]);
+            i++;
+        } else {
+            result.push(right[j]);
+            j++;
+        }
+    }
+
+    result = result.concat(left.slice(i)).concat(right.slice(j));
+
+    return result;
+}
+
 
 const unsortedArray = [3, 8, 4, 9, 2, 1, 8, 15, 5, 10, 11, 98]
 
